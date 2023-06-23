ebcc4a98eb0edbb1ac72f81714a84d25a85ca1a2
JADDYK <12012211@mail.sustech.edu.cn>
2023-06-23 19:11:04 +0800

    link

--- codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.d
+++ codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.d
@@ -1,7 +1,7 @@
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.rmeta: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.rmeta: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/libcfg_if-9279e08847ffee27.rlib: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/libcfg_if-9279e08847ffee27.rlib: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.d: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/cfg_if-9279e08847ffee27.d: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
 
-/home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
+/home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.d
+++ codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.d
@@ -1,7 +1,7 @@
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.rmeta: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.rmeta: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
 
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/libnb-ea209d5643ed05f6.rlib: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/libnb-ea209d5643ed05f6.rlib: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
 
-/home/yan_ice/Desktop/Teecert/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.d: /home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
+/home/wind/Desktop/TeernOS/codes/nk/target/riscv64imac-unknown-none-elf/release/deps/nb-ea209d5643ed05f6.d: /home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs
 
-/home/yan_ice/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs:
+/home/wind/.cargo/registry/src/github.com-1ecc6299db9ec823/nb-0.1.3/src/lib.rs:
